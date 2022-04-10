c289c3fb27792b29af4b88a9cfcbd9851ad7be93
milan Katira <84834795+milankatira@users.noreply.github.com>
2022-04-11 00:01:42 +0530

    complete product components

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -24,13 +24,19 @@ exports.getAllProducts = catchAsyncError(async (req, res) => {
   const apiFeature = new ApiFeatures(Product.find(), req.query)
     .search()
     .filter()
-    .pagination(resultPerPage);
 
   const products = await apiFeature.query;
+
+  let filteredProductsCount=products.length;
+
+    
+  apiFeature.pagination(resultPerPage);
   res.status(200).json({
     success: true,
     products,
     productCount,
+    resultPerPage,
+    filteredProductsCount
   });
 });
 
