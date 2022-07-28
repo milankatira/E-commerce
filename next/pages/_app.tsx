b87e0e881e8b7d1c1205b2b78cc16dc76ea446fc
milan Katira <84834795+milankatira@users.noreply.github.com>
2022-07-28 09:38:05 +0530

    initial commit

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -3,6 +3,8 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 // import Navbar from '../components/Navbar';
 import dynamic from 'next/dynamic';
+import Footer from '../components/common/layout/Footer';
+import Header from '../components/Header';
 const Navbar = dynamic(() => import('../components/common/layout/Navbar'), {
   ssr: false,
 });
@@ -11,7 +13,10 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute='class'>
       <Navbar />
+      <Header />
+
       <Component {...pageProps} />
+      <Footer/>
     </ThemeProvider>
   );
 }
